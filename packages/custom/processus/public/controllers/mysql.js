a24231a0dcaed8e4ea3fe7b08fd331c015b837f5
Gharbi Adlen <adlen025@gmail.com>
2015-06-08 00:07:01 +0100

    Front end mysql connect

--- packages/custom/processus/public/controllers/mysql.js
+++ packages/custom/processus/public/controllers/mysql.js
@@ -1,23 +1,39 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.processus').controller('MySQLController', ['$scope', 'Global', 'Processus', '$state',
-  function ($scope, Global, Processus, $state) {
+angular.module('mean.processus').controller('MySQLController', ['$scope', '$http', 'Global', 'Processus', '$state',
+  function ($scope, $http, Global, Processus, $state) {
     $scope.global = Global;
     $scope.package = {
       name: 'processus'
     };
 
     $scope.mysql = function () {
-      $state.go('configuration-mongodb');
+      var host = this.host || 'http://localhost/',
+        port = this.port || 3306,
+        user = this.user || 'root',
+        password = this.password,
+        database = this.dbName,
+        connection = { host: host, port: port, user: user, password: password, database: database };
+
+      $http.get('/api/connectmysql', { params: { connection: connection } }).success(function (tables) {
+        localStorage.setItem('connection', JSON.stringify(connection));
+        localStorage.setItem('tables', JSON.stringify(tables));
+        $state.go('configuration-mongodb');
+      }).error(function (params) {
+        // TODO
+      });
+
+
     };
-    
+
     $scope.mongodb = function () {
+
       $state.go('configuration-identification-associations');
     };
-    
+
     $scope.back = function () {
-      
+
     };
   }
 ]);
